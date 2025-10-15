bcb89ae7f8aefa14a1592d98797b7efa98042020
Aditya Sohal <adityasohal732@gmail.com>
2025-10-15 10:51:56 +0530

    adding routes of rrd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
+import { Layout } from 'lucide-react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='app' element={<Layout/>}/>
+        <Route path='/dashboard' index element={<Dashboard />} />
+        <Route path='/builder/:resumeID' element={<ResumeBuilder />} />
+        <Route/>
+        <Route path='/view/:resumeID' element={<Preview />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Resumebuilder = () => {
+  return (
+    <div>Resumebuilder</div>
+  )
+}
+
+export default Resumebuilder
