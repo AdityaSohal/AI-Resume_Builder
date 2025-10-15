4f8d5ae2ec737bc053760befbec9b2dcd07f6fbf
Aditya Sohal <adityasohal732@gmail.com>
2025-10-15 16:18:25 +0530

    changing colors of prebuilt components

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -22,14 +22,14 @@ const Hero = () => {
                     </a>
 
                     <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
-                        <a href="#" className="hover:text-indigo-600 transition">Home</a>
-                        <a href="#features" className="hover:text-indigo-600 transition">Features</a>
-                        <a href="#testimonials" className="hover:text-indigo-600 transition">Testimonials</a>
-                        <a href="#cta" className="hover:text-indigo-600 transition">Contact</a>
+                        <a href="#" className="hover:text-green-600 transition">Home</a>
+                        <a href="#features" className="hover:text-green-600 transition">Features</a>
+                        <a href="#testimonials" className="hover:text-green-600 transition">Testimonials</a>
+                        <a href="#cta" className="hover:text-green-600 transition">Contact</a>
                     </div>
 
                     <div className="flex gap-2">
-                        <a href="" className="hidden md:block px-6 py-2 bg-indigo-500 hover:bg-indigo-700 active:scale-95 transition-all rounded-full text-white">
+                        <a href="" className="hidden md:block px-6 py-2 bg-green-600 hover:bg-green-800 active:scale-95 transition-all rounded-full text-white">
                             Get started
                         </a>
                         <a href="" className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900">
@@ -50,14 +50,14 @@ const Hero = () => {
                     <a href="/products" className="text-white">Products</a>
                     <a href="/stories" className="text-white">Stories</a>
                     <a href="/pricing" className="text-white">Pricing</a>
-                    <button onClick={() => setMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square w-10 p-1 flex items-center justify-center bg-indigo-600 hover:bg-indigo-700 transition text-white rounded-md">
+                    <button onClick={() => setMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square w-10 p-1 flex items-center justify-center bg-green-600 hover:bg-green-800 transition text-white rounded-md">
                         X
                     </button>
                 </div>
 
                 {/* Hero Section */}
                 <div className="relative flex flex-col items-center justify-center text-sm px-4 md:px-16 lg:px-24 xl:px-40 text-black">
-                    <div className="absolute top-28 xl:top-10 -z-10 left-1/4 w-72 h-72 sm:w-96 sm:h-96 xl:w-[480px] xl:h-[480px] 2xl:w-[528px] 2xl:h-[528px] bg-indigo-300 blur-[100px] opacity-30"></div>
+                    <div className=" absolute top-28 xl:top-10 -z-10 left-1/4 w-72 h-72 sm:w-96 sm:h-96 xl:w-[480px] xl:h-[480px] 2xl:w-[528px] 2xl:h-[528px] bg-green-300 blur-[100px] opacity-30"></div>
 
                     {/* Avatars + Stars */}
                     <div className="flex items-center mt-24">
@@ -72,7 +72,7 @@ const Hero = () => {
                         <div>
                             <div className="flex">
                                 {Array(5).fill(0).map((_, i) => (
-                                    <svg key={i} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-star text-transparent fill-indigo-600" aria-hidden="true">
+                                    <svg key={i} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-star text-transparent fill-yellow-600" aria-hidden="true">
                                         <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
                                     </svg>
                                 ))}
@@ -85,7 +85,7 @@ const Hero = () => {
 
                     {/* Headline + CTA */}
                     <h1 className="text-5xl md:text-6xl font-semibold max-w-5xl text-center mt-4 md:leading-[70px]">
-                        Build stunning websites with <span className="bg-gradient-to-r from-indigo-700 to-indigo-600 bg-clip-text text-transparent whitespace-nowrap">PrebuiltUI</span> Components.
+                        Build stunning websites with <span className="bg-gradient-to-r from-green-800 to-green-600 bg-clip-text text-transparent whitespace-nowrap">PrebuiltUI</span> Components.
                     </h1>
 
                     <p className="max-w-md text-center text-base my-7">
@@ -94,7 +94,7 @@ const Hero = () => {
 
                     {/* CTA Buttons */}
                     <div className="flex items-center gap-4">
-                        <a href='/' className="bg-indigo-500 hover:bg-indigo-600 text-white rounded-full px-9 h-12 m-1 ring-offset-2 ring-1 ring-indigo-400 flex items-center transition-colors">
+                        <a href='/' className="bg-green-600 hover:bg-green-800 text-white rounded-full px-9 h-12 m-1 ring-offset-2 ring-1 ring-indigo-400 flex items-center transition-colors">
                             Get started
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right ml-1 w-4 h-4" aria-hidden="true">
                                 <path d="M5 12h14"></path>
