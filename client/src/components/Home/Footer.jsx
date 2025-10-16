e58f3fd10874e9ccb18c690f08f90137508350de
Aditya Sohal <adityasohal732@gmail.com>
2025-10-16 11:01:06 +0530

    made changes in footer

--- client/src/components/Home/Footer.jsx
+++ client/src/components/Home/Footer.jsx
@@ -69,7 +69,7 @@ export default function Example() {
                     <div className="flex items-center gap-4 mt-3">
                         {/* Social Icons */}
                         {/* Dribbble */}
-                        <a href="https://dribbble.com/prebuiltui" target="_blank" rel="noreferrer">
+                        <a href="https://dribbble.com" target="_blank" rel="noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-dribbble size-5 hover:text-green-500" aria-hidden="true">
                                 <circle cx="12" cy="12" r="10"></circle>
                                 <path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94"></path>
@@ -78,7 +78,7 @@ export default function Example() {
                             </svg>
                         </a>
                         {/* LinkedIn */}
-                        <a href="https://www.linkedin.com/company/prebuiltui" target="_blank" rel="noreferrer">
+                        <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin size-5 hover:text-green-500" aria-hidden="true">
                                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                                 <rect width="4" height="12" x="2" y="9"></rect>
@@ -86,13 +86,13 @@ export default function Example() {
                             </svg>
                         </a>
                         {/* Twitter */}
-                        <a href="https://x.com/prebuiltui" target="_blank" rel="noreferrer">
+                        <a href="https://x.com" target="_blank" rel="noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-twitter size-5 hover:text-green-500" aria-hidden="true">
                                 <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                             </svg>
                         </a>
                         {/* YouTube */}
-                        <a href="https://www.youtube.com/@prebuiltui" target="_blank" rel="noreferrer">
+                        <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-youtube size-6 hover:text-green-500" aria-hidden="true">
                                 <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"></path>
                                 <path d="m10 15 5-3-5-3z"></path>
