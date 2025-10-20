b516e6e4cf964d1cc77c05663c180f3ba2e37294
Aditya Sohal <adityasohal732@gmail.com>
2025-10-20 23:48:01 +0530

    making skill form

--- client/src/components/SkillForm.jsx
+++ client/src/components/SkillForm.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SkillForm = () => {
+const SkillForm = (data, onChange ) => {
+    const[newSkill , setNewSkill] = useState("")
+    const addSkill = ()=>{
+        if(newSkill.trim() && !data.includes(newSkill.trim())){
+            onChange([...data,newSkill.trim()])
+            setNewSkill("")
+        }
+    }
+    const removeSkill = (indexToRemove)=>{
+        onChange(data.filter((_,index)=>index!=indexToRemove))
+    }
+    const handleKeyPress= (e)=>{
+        if(e.key === "Enter"){
+            e.preventDefault();
+            addSkill();
+        }
+    }
   return (
     <div>SkillForm</div>
   )
