2dda97937bd0c7d6fda1c611e67b68883a252fa6
Aditya Sohal <adityasohal732@gmail.com>
2025-10-21 23:46:58 +0530

    added the database and started making the schema using mongodb also started maing user registerations

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import connectDB from './configs/db.js';
+
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+await connectDB()
 
 app.use(express.json())
 app.use(cors())
