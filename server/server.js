ab53f3c9731de0f984ea70b9b870fbc343a39700
Aditya Sohal <adityasohal732@gmail.com>
2026-03-18 23:39:03 +0530

    changed server code

--- server/server.js
+++ server/server.js
@@ -16,14 +16,7 @@ const allowedOrigins = [
     process.env.FRONTEND_URL
 ].filter(Boolean)
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true)
-        if (allowedOrigins.includes(origin)) return callback(null, true)
-        callback(new Error('Not allowed by CORS'))
-    },
-    credentials: true
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
